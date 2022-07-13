2649ebb715e1028e842afcc339eabfe1bd19ee88
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-07-13 18:04:07 -0300

    :sparkles: | Python modules environment

---
+++ src/test_core/dbProva.py
@@ -0,0 +1,160 @@
+#-*- coding: utf-8 -*-
+try:
+    import sqlite3
+    from util import *
+except Exception as e:
+    print(e)
+#Trecho que inicia o banco
+try:
+    nome_banco = 'database.db'
+    banco = sqlite3.connect(nome_banco)
+    cursor = banco.cursor()
+except sqlite3 as error:
+    print(f'[ERRO] NÃO FOI POSSÍVEL INICIAR O BD -> {error} <-')
+
+
+
+def dadosProva(id_prova, basedir = '', id_alunos = None):
+    """
+    Dado id de uma prova, a função retorna:
+        *O nome desse prova
+        *O nome do professor relacionado à essa prova
+        *O nome da turma relacionada à essa prova
+        *A data de realização dessa provas
+        *A quantidade de questões nessa prova
+        *A quantidade de alternativas nessa prova
+        *O nome dos alunos que estão relacionados a essa prova
+        *O id de cada aluno
+        *O id do usuário que está gerando a prova
+
+
+    @parâmetro id_prova Identificador da prova a ser gerada
+    @parâmetro basedir Local onde será salvo o arquivo, caso não seja informado
+    esse campo receberá <vazio>
+    @parâmetro id_alunos Id do aluno ao qual de qual a prova será gerada
+        (a)None: Serão passados os dados de todos os alunos relacionados àquela
+        prova
+        (b)Se for passada uma lista com apenas um valor inteiro, serão passados
+        os dados apenas do aluno com o id correspondente
+        (c)Se for passada uma lista com mais de um valor inteiro, serão passados
+        os dados de todos os alunos relacionados aos ids correspondentes
+
+    """
+
+
+    try:
+        #Seleciona tudo de uma linha de prova
+        cursor.execute('SELECT * FROM prova WHERE id_prova=?', [id_prova])
+        dados_prova = cursor.fetchall()
+        #Seleciona tudo de uma linha de turma
+        cursor.execute(f'SELECT * FROM turma WHERE id_turma=?', [dados_prova[0][5]])
+        dados_turma = cursor.fetchall()
+        #Seleciona o nome de uma linha de usuario
+        cursor.execute(f'SELECT nome, id_usuario FROM usuario WHERE id_usuario=?', [dados_turma[0][3]])
+        dados_usuario = cursor.fetchall()
+        #Seleciona id_aluno de uma linha de turma (porque uma turma toda vai estar relacionada a uma prova)
+        cursor.execute(f'SELECT id_aluno FROM aluno_turma WHERE id_turma=?',[dados_prova[0][5]])
+        id_aluno_prova = cursor.fetchall()
+        #Seleciona peso de uma linha de gabarito
+        cursor.execute(f'SELECT peso FROM gabarito WHERE id_prova=?',[id_prova])
+        pesos_gabarito = cursor.fetchall()
+        nome_prova = dados_prova[0][1]
+        nome_professor = dados_usuario[0][0]
+        id_usuario = dados_usuario[0][1]
+        nome_turma = dados_turma[0][1]
+        data = dados_prova[0][2]
+        qtd_q = dados_prova[0][3]
+        qtd_a = dados_prova[0][4]
+        #Calculando a nota total da prova
+        nota_prova = 0
+        for i in range(len(pesos_gabarito)):
+            nota_prova += pesos_gabarito[i][0]
+
+
+        #Trecho que verifica quais alunos irão aparecer
+        #Se None == Todos
+        #Se type is int vai receber um aluno específico
+        #Se type is list vai receber determinados alunos
+        if id_alunos == None:
+            id =  idRepetido(id_aluno_prova)
+
+
+        else:
+            id = []
+            if type(id_alunos) is int:
+                id.append(id_alunos)
+            else:
+                for i in id_alunos:
+                    id.append(i)
+
+        #Lista que vai receber nomes de alunos pura com 3 dimensoes
+        lista_alunos = []
+        #Lista que vai receber nomes de alunos com 1 dimensão
+        nome_alunos = []
+        for i in id:
+            cursor.execute(f'SELECT nome_aluno FROM aluno WHERE id_aluno=?',[i])
+            lista_alunos.append(cursor.fetchall())
+        for i in range(len(id)):
+                nome_alunos.append(lista_alunos[i][0][0])
+
+        #print(nome_alunos)
+        return nome_prova, nome_professor, nome_turma, data, qtd_q, qtd_a, nota_prova, nome_alunos, id, id_usuario
+
+    except sqlite3 as error:
+        print(f'[ERRO] NÃO FOI POSSÍVEL EXIBIR PROFESSORES CADASTRADOS -> {error} <-')
+
+def obterProva(id_prova, id_aluno):
+    """
+    Dado id de uma prova e de um aluno, a função retorna:
+        *O gabarito dessa prova
+        *As respostas preenchidas nessa prova
+
+    @parâmetro id_prova Identificador da prova a ser buscada
+    @parâmetro id_aluno Identificador do aluno a ser buscado
+    """
+    #Número de questões, número de alternativas e gabarito
+    if id_aluno != None:
+        try:
+            cursor.execute('SELECT n_questao, opcao FROM aluno_prova WHERE id_prova=? AND id_aluno=?', [id_prova, id_aluno])
+            prova = cursor.fetchall()
+
+            cursor.execute('SELECT n_questao, opcao, peso FROM gabarito WHERE id_prova=?', [id_prova])
+            gabarito = cursor.fetchall()
+
+            #Eu estou usando o "[0][0] no final, pra ele retorna, por exemplo, apenas, 4 e não [(4,)]"
+            #Isso parece válido, já que sempre teremos um valor para cada
+            cursor.execute('SELECT n_questoes FROM prova WHERE id_prova=?', [id_prova])
+            n_q = cursor.fetchall()[0][0]
+
+            cursor.execute('SELECT n_alternativas FROM prova WHERE id_prova=?', [id_prova])
+            n_alt = cursor.fetchall()[0][0]
+
+        except sqlite3 as error:
+            print(f'[ERRO] NÃO FOI POSSÍVEL LOCALIZAR A PROVA -> {error} <-')
+
+    return prova, gabarito, n_q, n_alt
+
+def obterQuestoesProva(id_prova, id_aluno):
+    """
+    Dado id de uma prova e de um aluno, a função retorna:
+        *O gabarito dessa prova
+        *As respostas preenchidas nessa prova
+
+    @parâmetro id_prova Identificador da prova a ser buscada
+    @parâmetro id_aluno Identificador do aluno a ser buscado
+    """
+    #Número de questões, número de alternativas e gabarito
+    if id_aluno != None:
+        try:
+            #Eu estou usando o "[0][0] no final, pra ele retorna, por exemplo, apenas, 4 e não [(4,)]"
+            #Isso parece válido, já que sempre teremos um valor para cada
+            cursor.execute('SELECT n_questoes FROM prova WHERE id_prova=?', [id_prova])
+            n_q = cursor.fetchall()[0][0]
+
+            cursor.execute('SELECT n_alternativas FROM prova WHERE id_prova=?', [id_prova])
+            n_alt = cursor.fetchall()[0][0]
+
+        except sqlite3 as error:
+            print(f'[ERRO] NÃO FOI POSSÍVEL LOCALIZAR A PROVA -> {error} <-')
+
+    return n_q, n_alt
---
+++ src/test_core/funcaoProva.py
@@ -0,0 +1,125 @@
+try:
+    import cv2
+    import numpy as np
+    import os
+    import os.path
+    import sys
+    from fpdf import FPDF
+    from PIL import ImageFont, ImageDraw, Image
+    from libQr import *
+    from dbProva import *
+except Exception as e:
+    print(e)
+
+def gerar_prova(id_prova, basedir ='.', id_alunos = None):
+    #Quando eu comecei a trabalhar no terminal do Ubuntu ele dizia que essa variável não havia sigo criada, então eu a criei aqui
+    path = ''
+
+    try:
+        #Verifica se a pasta 'provas' já existe
+        #Se não existir, ela será criada
+        if(not os.path.isdir(f'{basedir}/')):
+            os.mkdir(f'{basedir}/')
+
+        #nome_prova, nome_professor, nome_turma, data, qtd_q, qtd_a, nome_alunos
+        prova, prof, turma, data, qtd_quadrado_v, qtd_quadrado_h, nota_prova, aluno, id_aluno, id_usuario = dadosProva(id_prova, basedir, id_alunos)
+        pdf = FPDF(format = (int(1240/2.7),int(1754/2.7))) #Página definda para o tamanho da imagem
+        for m in range(len(aluno)):
+            img = np.ones((1754,1240,3),np.uint8)*255 #imagem 1754x1240, com fundo branco e 3 canais para as cores
+
+            altura = img.shape[0]
+            largura = img.shape[1]
+
+            fonte = cv2.FONT_HERSHEY_SIMPLEX
+            escala = 0.7
+            espessura = 2
+
+            #Desenha linha e
+            img = cv2.line(img,(160,270),(largura-160,270),(0,0,0),1)
+            #Segunda linha
+            img = cv2.line(img,(80,560),(largura-80,560),(0,0,0),2)
+            #Desenha retângulos das notas
+            cv2.rectangle(img, (330, 320), (580, 470), (0, 0, 0), 2)
+            cv2.rectangle(img, (largura-330, 320), (largura-580, 470), (0, 0, 0), 2)
+
+            #Desenha as informações do cabecalho
+            img = desenhaCabecalho(img, largura, altura, aluno[m], prof, prova, turma, data, nota_prova)
+
+            #Gera o qr
+            #Nº da prova . Nº do aluno
+            msg = formataQr(f'{id_prova}.{id_aluno[m]}')
+            qr_code = escreveQr(msg)
+
+            #Coloca o qr na imagem
+            img[0 : qr_code.shape[0], largura-qr_code.shape[1] : largura] = qr_code
+
+            #Apaga qrcode depois de adicioná-lo ao pdf
+            os.unlink(f'qr.png')
+
+            #Valors originais dos topos dos triângulos (último campo, posição x): 40,largura-70, 40,
+            #Cria marcadores triângulo
+            t1 = np.array([[40,640],[70,640],[40,610]], np.int32)
+            t2 = np.array([[largura-70,640],[largura-40,640],[largura-70,610]], np.int32)
+            t3 = np.array([[40,altura-60],[70,altura-60],[40,altura-90]], np.int32)
+            t4 = np.array([[largura-70,altura-60],[largura-40,altura-60],[largura-70,altura-90]], np.int32)
+            t = [t1, t2, t3, t4]
+            for i in range(len(t)):
+                cv2.fillPoly(img, [t[i]], (0, 0, 0))
+
+            #Coloca os marcadores quadrados na vertical
+            espaco = int(0)
+            cinza = (210, 210, 210)
+            for i in range(qtd_quadrado_v):
+                cv2.rectangle(img, (120,700 + espaco), (140, 720 + espaco), (0, 0, 0), -1)
+                cv2.putText(img, f'{i+1}', (150,720 + espaco), fonte, escala, cinza, espessura)
+                espaco += 45
+
+            #Coloca os marcadores quadrados na horizontal
+            espaco = 0
+            for i in range(qtd_quadrado_h):
+                cv2.rectangle(img, (260 + espaco,650), (280 + espaco, 670), (0, 0, 0), -1)
+                espaco += 120
+
+            #Colocando os círculos e letras
+            letras = ['A','B','C','D','E','F','G']
+            espaco_x = espaco_y = 0
+            for i in range(qtd_quadrado_v):
+                for j in range(qtd_quadrado_h):
+                    cv2.circle(img, (270 + espaco_x, 710 + espaco_y), 14, cinza,2)
+                    cv2.putText(img, f'{letras[j]}', (263 + espaco_x, 717 + espaco_y), fonte, escala, cinza, espessura)
+                    espaco_x += 120
+                espaco_x = 0
+                espaco_y += 45
+
+            #Redminsionando a imagem temporiamente apenas para os teste
+            #img_new_h = resizeImg(img, 840)
+
+            #cv2.imshow("Canvas", img)
+            #cv2.waitKey(0)
+            cv2.imwrite(f'{basedir}/prova{m}.png', img)
+
+            #Cria nova página, quebra a página, busca a imagem
+            pdf.add_page()
+            pdf.set_auto_page_break(0)
+            pdf.image(f'{basedir}/prova{m}.png')
+
+            #Apaga imagem depois de adicioná-la ao pdf
+            os.unlink(f'{basedir}/prova{m}.png')
+
+        pdf.output(f'{basedir}/prova{id_usuario}.pdf', "F")
+        #COMANDO QUE EXCLUI O PDF#
+        path = os.path.abspath(f'{basedir}/prova{id_usuario}.pdf')
+    except Exception as e:
+        print(e)
+
+    return path.replace("\\", "/")
+
+#python[-1] file.py[0] id_prova[1] id_alunos[2]
+try:
+    if (sys.argv[2] != '0'):
+        path = gerar_prova(id_prova = sys.argv[1], id_alunos = sys.argv[2].split(','))
+    else:
+        path = gerar_prova(id_prova = sys.argv[1])
+    print(path)
+except Exception as e:
+    print(e)
